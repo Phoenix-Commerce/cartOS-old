df2b4fd9c0ef2d70c9901b8a87fedf998be445fc
Brent Hoover <brent@thebuddhalodge.com>
2024-01-05 15:28:26 +0800

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DryerModule } from 'dryerjs';
+
+import { User } from './models/user';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/cartos'),
+    DryerModule.register({ definitions: [User] }),
+  ],
+})
+export class AppModule {}
